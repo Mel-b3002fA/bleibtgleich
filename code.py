64b372cb6abfc5b46cc9f52e4d2c4a8ec65e5b2a
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-03-11 19:12:08 +0100

    push

--- code.py
+++ code.py
@@ -2,3 +2,8 @@
 from transformers import pipeline
 
 pipe = pipeline("fill-mask", model="google-bert/bert-base-multilingual-cased")
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+
+tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-multilingual-cased")
+model = AutoModelForMaskedLM.from_pretrained("google-bert/bert-base-multilingual-cased")
