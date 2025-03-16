de324571a7febfd722b81cd9178d8a17f87092dc
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-03-16 08:01:38 +0100

    push

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-# Use a pipeline as a high-level helper
+""" # Use a pipeline as a high-level helper
 from transformers import pipeline
 
 pipe = pipeline("fill-mask", model="google-bert/bert-base-multilingual-cased")
@@ -6,4 +6,4 @@ pipe = pipeline("fill-mask", model="google-bert/bert-base-multilingual-cased")
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 
 tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-multilingual-cased")
-model = AutoModelForMaskedLM.from_pretrained("google-bert/bert-base-multilingual-cased")
+model = AutoModelForMaskedLM.from_pretrained("google-bert/bert-base-multilingual-cased") """
